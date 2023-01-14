62be0121effbf340c3f17384253f05e0016461a3
germano <germanomacieira@hotmail.com>
2023-01-14 11:25:13 -0300

    Add: service

--- src/main/java/br/unifor/apispringsecurity/service/Impl/StudentImpl.java
+++ src/main/java/br/unifor/apispringsecurity/service/Impl/StudentImpl.java
@@ -1,5 +1,6 @@
 package br.unifor.apispringsecurity.service.Impl;
 
+
 import br.unifor.apispringsecurity.model.Student;
 import br.unifor.apispringsecurity.repository.StudentRepository;
 import br.unifor.apispringsecurity.service.StudentService;
@@ -7,16 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentImpl implements StudentService {
 
     @Autowired
-    private StudentRepository StudentRepository;
+    private StudentRepository studentRepository;
 
     @Override
     public List<Student> listStudents() {
 
-        return this.StudentRepository.findAll();
+        return this.studentRepository.findAll();
+    }
+
+    @Override
+    public Student listStudentById(long idStudent) {
+        Optional<Student> optionalStudent = this.studentRepository.findById(idStudent);
+        return new Student(optionalStudent.get());
+    }
+
+    @Override
+    public Student insertStudent(Student student) {
+        return this.studentRepository.save(student);
     }
 }
--- src/main/java/br/unifor/apispringsecurity/service/StudentService.java
+++ src/main/java/br/unifor/apispringsecurity/service/StudentService.java
@@ -1,12 +1,13 @@
+
 package br.unifor.apispringsecurity.service;
 
 import br.unifor.apispringsecurity.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface StudentService {
     List<Student> listStudents();
+    public Student listStudentById(long idStudent);
+    public Student insertStudent(Student student);
 
 }
